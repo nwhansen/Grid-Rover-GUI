f5004eb810eeb8841460b98d3835f5ce68822a88
Cody Foltz <codablo@gmail.com>
2012-04-17 16:23:12 -0600

    namespace

--- Model/include/Thing.h
+++ Model/include/Thing.h
@@ -1,25 +1,54 @@
 /* 
- * File:   Thing.h
+ * File:   AbstractThing.h
  * Author: Cody Foltz
  *
- * Created on April 5, 2012, 12:57 PM
+ * Created on April 5, 2012, 12:55 PM
  */
 
 #ifndef THING_H
 #define	THING_H
 
-#include "AbstractThing.h"
+#include <string>
+#include <vector>
 
-using namespace AbstractThingNameSpace;
+using namespace std;
 
-namespace ThingNameSpace{
-    
-    class Thing : AbstractThing{
+namespace Model{
+
+    class Thing{
+
+    protected:
         
-    private:
+        string name;
+        int weight;
+        int mass;
+        int density;
+        int volume;
+        int collectPointValue;
+        int discoverPointValue;
+        string discription;
         
+        //Set Methods
+        SetWeight(int weight){this->weight = weight;};
+        SetMass(int mass){this->mass = mass;};
+        SetDensity(int density){this->density = density;};
+        SetCollectPointValue(int collectPointValue){this->collectPointValue =collectPointValue;};
+        SetDiscoverPointValue(int discoverPointValue){this->discoverPointValue = discoverPointValue;};
+        SetVolume(int volume){this->volume = volume;};
+        SetName(string name){this->name = name;};
         
-    public :
+    public:
+        //Get Methods
+        int GetWeight(){return weight;};
+        int GetMass(){return mass;};
+        int GetDensity(){return density;};
+        int GetVolume(){return volume;};
+        int GetCollectPointValue(){return collectPointValue;};
+        int GetDiscoverPointValue(){return discoverPointValue;};
+        string GetThingDiscription(){return discription;};
+        string GetName(){return name;};
+
+        //Constructors
         Thing();
         Thing(int mass, int density, int collectPointValue, int discoverPointValue);
 
@@ -30,7 +59,6 @@ namespace ThingNameSpace{
         // 3 = disaster was created. ie. Rock hit rover, thing exploded in rover's "face";
         virtual int Tweak() = 0;
     };
+
 }
-
 #endif	/* THING_H */
-
