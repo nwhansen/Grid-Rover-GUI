7b9be070e7fffb7b23f92b90d9eef9cd4bd52420
Lucas Adam M. Paul <adamp@eng.utah.edu>
2012-04-17 18:07:13 -0600

    Added GetCommandEvent

---
+++ Model/GetCommandEvent.C
@@ -0,0 +1,48 @@
+/*
+ * File:   GetCommandEvent.C
+ * Author: Lucas Adam M. Paul
+ *
+ * Created on 2012/04/17 16:44:24
+ */
+
+#include <stdio.h>
+#include "Event.h"
+#include "TitanTime.h"
+#include "Engine.h"
+#include "GetCommandEvent.h"
+#include "Rover.h"
+#include "RoverInterface.h"
+#include "Result.h"
+#include "Communication.h"
+
+namespace Model {
+
+    ResultType GetCommandEvent::fire() {
+    	bool valid = false;
+    	Rover* rover = engine->GetRover(0);
+        RoverInterface* ri = rover->GetRoverInterface();
+		Communication comm = ri->RecieveFormattedMessage();
+		if (comm.command.compare("move") == 0) {
+			if (comm.arguments.size > 0 && comm.arguments.front().length() > 0) {
+				char direction = tolower(comm.arguments.front()[0]);
+				if (direction == 'e' || direction == 'n' || direction == 's' || direction == 'w') {
+					Titan::TitanTime traveltime(0,0,20);
+					engine->AddEvent(MoveEvent(m, time + traveltime, rover, direction));
+					valid = true;
+				}
+			}
+		}
+		else if (comm.command.compare("look") == 0) {
+		}
+		else if (comm.command.compare("selfdestruct") == 0) {
+			engine->EndGame();
+		}
+		if (!valid) {
+			Titan::TitanTime penaltytime(0,0,1);
+			engine->AddEvent(GetCommandEvent(m, time + penaltytime);
+			return Fail;
+		}
+        return Get;
+    }
+
+}
