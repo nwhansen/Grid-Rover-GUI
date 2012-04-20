b2318fa5991eee9000119de5d5edff4b5b525503
Cody Foltz <codablo@gmail.com>
2012-04-20 13:19:54 -0600

    working on file reading

--- Model/ThingFactory.C
+++ Model/ThingFactory.C
@@ -47,14 +47,14 @@ Thing ThingFactory::GetRandomThing(){
 bool ThingFactory::ReadFromFile(vector<string> rawThings){
     
         string data;
-	ifstream infile;
-	infile.open (objectFile.c_str());
-        while(!infile.eof) 
-        {
-	        getline(infile,data); 
-                rawThings.push_back(data);
-        }
-	infile.close();
+//	ifstream infile;
+//	infile.open (objectFile.c_str());
+//      while(!infile.eof) 
+//        {
+//	        getline(infile,data); 
+//                rawThings.push_back(data);
+//        }
+//	infile.close();
 }
 void ThingFactory::CreateThingFromVector(vector<string> rawThings){
     
@@ -79,21 +79,20 @@ Thing ThingFactory::ParseRawThingData(string rawThingData){
     
     startIndex = rawThingData.find("id=");
     endIndex = rawThingData.find("", startIndex);
-    id = rawThingData.substr(startIndex, endIndex-startIndex+1);
+//    id = rawThingData.substr(startIndex, endIndex-startIndex+1);
     
     startIndex = rawThingData.find("volume=");
     endIndex = rawThingData.find("", startIndex);
-    volume = rawThingData.substr(startIndex, endIndex- startIndex+1);
+//    volume = rawThingData.substr(startIndex, endIndex- startIndex+1);
     
     startIndex = rawThingData.find("collectpoint=");
     endIndex = rawThingData.find("", startIndex);
-    collectPoints = rawThingData.substr(startIndex, endIndex-startIndex+1);
+//    collectPoints = rawThingData.substr(startIndex, endIndex-startIndex+1);
     
     startIndex = rawThingData.find("discoverpoint=");
     endIndex = rawThingData.find("", startIndex);
-    discoverPoints = rawThingData.substr(startIndex, endIndex-startIndex+1);
+//    discoverPoints = rawThingData.substr(startIndex, endIndex-startIndex+1);
     
-    rawThingData.find
 }
 
 //ThingsData.txt
